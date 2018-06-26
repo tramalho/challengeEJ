6946594b12440469ad165fa2e8849ca585e995fd
tramalho <tramalho@ciandt.com>
2018-06-25 21:51:40 -0300

    init img feature

--- app/src/main/java/br/com/tramalho/enjoeitest/presentation/BindingUtils.kt
+++ app/src/main/java/br/com/tramalho/enjoeitest/presentation/BindingUtils.kt
@@ -3,16 +3,26 @@ package br.com.tramalho.enjoeitest.presentation
 import android.databinding.BindingAdapter
 import android.databinding.ObservableArrayList
 import android.support.v7.widget.RecyclerView
-import android.view.View
+import android.text.TextUtils
+import android.widget.ImageView
+import br.com.tramalho.enjoeitest.R
 
-
-@BindingAdapter("android:visibility")
-fun setVisibility(view: View, value: Boolean) {
-    view.setVisibility(if (value) View.VISIBLE else View.GONE)
-}
 
 @BindingAdapter("bind:itens")
 fun <T> bindItens(recyclerView: RecyclerView, list: ObservableArrayList<T>) {
     val adapter = recyclerView.adapter as CustomAdapter<T>
     adapter.updateItens(list)
+}
+
+@BindingAdapter("imageUrl")
+fun loadImage(imageView: ImageView, url: String) {
+    val context = imageView.contextd
+    if (!TextUtils.isEmpty(url)) {
+        Glide.with(context)
+                .load(url)
+                .roundedCorners(context, 5)
+                .into(imageView)
+    } else {
+        imageView.setImageResource(R.drawable.hint_img)
+    }
 }
